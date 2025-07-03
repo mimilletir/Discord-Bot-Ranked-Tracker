31658cb6e0a12f2245029a8f303d51a6cd215b41
Milan Dupuis <milan.dupuis@gmail.com>
2025-07-03 19:31:33 +0200

    ADD Channel Initialisation

--- DiscordBotTFT.Bots/Commands/MatchStatusCommands.cs
+++ DiscordBotTFT.Bots/Commands/MatchStatusCommands.cs
@@ -1,5 +1,7 @@
 ﻿using DiscordBotTFT.Core.Services.Profiles;
+using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.SlashCommands;
+using Newtonsoft.Json;
 
 namespace DiscordBotTFT.Bots.Commands
 {
@@ -10,6 +12,30 @@ namespace DiscordBotTFT.Bots.Commands
         public MatchStatusCommands(IProfileService profileService)
         {
             _profileService = profileService;
+        }
+
+        [SlashCommand("_init_", "initialise le bot dans le channel où la commande est lancée")]
+        [RequireRoles(RoleCheckMode.Any, "Admin")]
+        public async Task Init(InteractionContext ctx)
+        {
+            string filePath = "configChannels.json";
+            Dictionary<ulong, ulong> channelMap = new();
+
+            string json = await File.ReadAllTextAsync(filePath);
+            channelMap = JsonConvert.DeserializeObject<Dictionary<ulong, ulong>>(json)
+                            ?? new Dictionary<ulong, ulong>();
+
+            channelMap[ctx.Guild.Id] = ctx.Channel.Id;
+
+            string updatedJson = JsonConvert.SerializeObject(channelMap, Formatting.Indented);
+            await File.WriteAllTextAsync(filePath, updatedJson);
+
+            await ctx.CreateResponseAsync("Initialisation réussi !");
+        }
+
+        public async Task CheckMatchStatus(InteractionContext ctx)
+        {
+
         }
     }
 }
